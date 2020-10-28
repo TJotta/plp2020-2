87878fffafdbc8832e38c0331cc3ca8f3e46b656
TJotta <73568358+TJotta@users.noreply.github.com>
2020-10-27 23:13:03 -0300

    Add files via upload

---
+++ EXE1/EXE1.hs
@@ -0,0 +1,12 @@
+-- 1. Soma quadrado do dobro 
+quadradoDobro n1 = (n1 + n1)^2
+
+-- 2. Soma dobro do quadrado 
+dobroQuadrado n1 = (n1^2) * 2
+
+-- 3. Média aritmetica 
+mediaAritmetica nota1 nota2 nota3 = (nota1 + nota2 + nota3) / 3 
+
+-- 4. Salario minimo e Quilowatts
+salarioQuilowatts s q =  let q = (1/5) * s
+                            in q * 0.15
